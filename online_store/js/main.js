495d8c8dc884b198f252704fad93affd4f10b9ac
nazeef <36044106+nazeefnezzi@users.noreply.github.com>
2019-09-25 15:41:34 +0200

    Add files via upload

---
+++ online_store/js/main.js
@@ -0,0 +1,29 @@
+/*first slide */
+
+$('.slider-one')
+.not('.slick-intialized')
+.slick({
+   autoplay:true,
+   autoplaySpeed:3000,
+   dots:true,
+   prevArrow:"site-slider .slider-btn .prev",
+   nextArrow:"site-slider .slider-btn .next"
+
+   
+
+});
+
+/* second slider */
+
+$('.slider-two')
+.not('.slick-intialized')
+.slick({
+   
+   prevArrow:"site-slider-two .prev",
+   nextArrow:"site-slider-two .next",
+   slidesToShow:5,
+   slidesToScroll:1,
+   autoplaySpeed:3000
+   
+
+});
